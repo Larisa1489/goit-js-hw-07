bf152329bb9461a3b0c0047d50494e9cee6eb678
Larisa1489 <107617712+Larisa1489@users.noreply.github.com>
2024-03-21 11:54:55 +0200

    js7

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,11 @@
-const galleryList = document.querySelectorAll('gallery');
-images.forEach(img => {
-  const listItem = document.createElement('li');
-  const imgElement = document.createElement('img');
-  imgElement.src = img.url;
-  imgElement.alt = img.alt;
-  listItem.appendChild(imgElement);
-  galleryList.appendChild(listItem);
-  console.log(galleryList);
+const categoriesList = document.querySelectorAll('ul#categories .item');
+
+console.log(`Number of categories: ${categoriesList.length}`);
+
+categoriesList.forEach(category => {
+  const categoryName = category.querySelector('h2').textContent;
+  const categoryItemsCount = category.querySelectorAll('ul li').length;
+
+  console.log(`Category: ${categoryName}`);
+  console.log(`Number of items: ${categoryItemsCount}`);
 });
